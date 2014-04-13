419301a649acb5c9343bdb5bd55aea3ef404abfc
Melanie Murray <murray.melanief@gmail.com>
2014-04-13 09:11:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DroidSpy::Application.config.secret_key_base = 'bef82098b61ff12f3653d8298b2397c81a8b974bc0f229298a7648411d157972bb6cc822d178b617dec823598adcf621a70fb9b947e6652b2af90d3cac293af3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DroidSpy::Application.config.session_store :cookie_store, key: '_DroidSpy_session'
